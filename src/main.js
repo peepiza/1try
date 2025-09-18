987f6ab85930fac18043ca8a801d3f3369160b6c
Elizabeth Laricheva <li3ywa@gmail.com>
2025-09-18 09:29:54 +0300

    ю

--- src/main.js
+++ src/main.js
@@ -6,13 +6,15 @@ form?.addEventListener('submit', (e) => {
         const email = form.elements.email; 
         if (email?.validity.typeMismatch) {
             email.setCustomValidity('Введите корректный e-mail, например name@example.com');
-        }
-        form.reportValidity(); 
-        [...form.elements].forEach(el => {
-            if (el.willValidate) el.toggleAttribute('aria-invalid', !el.checkValidity());
-        });
-        return;
-    } 
-    e.preventDefault();
-    document.getElementById('contactDialog')?.close('success'); form.reset();
+}
+form.reportValidity(); 
+
+[...form.elements].forEach(el => {
+    if (el.willValidate) el.toggleAttribute('aria-invalid', !el.checkValidity());
+});
+return;
+} 
+e.preventDefault();
+document.getElementById('contactDialog')?.close('success'); 
+form.reset();
 });
